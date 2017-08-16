dfd943ca931e701975a3a4defea6451ede50e11d
st135 <st135yle@gmail.com>
2017-08-16 21:04:40 +0900

    170816 Save Work

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name= 'polls'
+
 urlpatterns = [
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
